e30221238cb5b5fc733503a82a6c0f61ba568f6b
Deepali <deepalimusale3@gmail.com>
2020-10-19 23:19:47 -0600

    Added Browser Router snippet to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<browserRouter basename={window.location.pathname || ''}>
+    {/* <App /> */}
+    <Route exact path='/' component={index} />
+</browserRouter>, 
+document.getElementById('root'));
 registerServiceWorker();
