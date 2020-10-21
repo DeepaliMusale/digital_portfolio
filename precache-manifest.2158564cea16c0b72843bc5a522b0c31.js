0335ce4c0640f58cb9a0bb8fb8bc0e270ea512e1
Deepali <deepalimusale3@gmail.com>
2020-10-21 17:56:13 -0600

    Updates

---
+++ precache-manifest.2158564cea16c0b72843bc5a522b0c31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2be54ceaa34f5c07bc998198a4958e",
+    "url": "https://deepalimusale.github.io/digital_portfolio/index.html"
+  },
+  {
+    "revision": "821697ec351c63194524",
+    "url": "https://deepalimusale.github.io/digital_portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "793d4fd395712e3af546",
+    "url": "https://deepalimusale.github.io/digital_portfolio/static/js/2.b952bf89.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "https://deepalimusale.github.io/digital_portfolio/static/js/2.b952bf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "821697ec351c63194524",
+    "url": "https://deepalimusale.github.io/digital_portfolio/static/js/main.58fd721b.chunk.js"
+  },
+  {
+    "revision": "74fafb9f4eb9689d3cf6",
+    "url": "https://deepalimusale.github.io/digital_portfolio/static/js/runtime-main.cf74cc53.js"
+  }
+]);
